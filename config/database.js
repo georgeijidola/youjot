5fd99d78cfcbc4ef4af4499d0ac4a4b192fa6349
George Ijidola <geordola@gmail.com>
2019-01-31 18:56:33 +0100

    App ready for deployment

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 if (process.env.NODE_ENV === "production") {
   module.exports = {
-    mongoURI: ""
+    mongoURI: "mongodb://<dbuser>:<dbpassword>@ds117535.mlab.com:17535/youjot-prod"
   }
 } else {
   module.exports = { 
